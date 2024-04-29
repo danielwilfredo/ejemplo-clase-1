b9ff3cba537345fdd2b4f1522937da45e7fdbffb
Daniel Granados <xvwilito11@gmail.com>
2024-04-29 10:58:33 -0600

    Add files via upload

---
+++ Boton.js
@@ -0,0 +1,24 @@
+import {TouchableOpacity, Text, StyleSheet} from 'react-native';
+
+export default function Button({texto, funcion, colorEnviado}){
+
+    return(
+        <TouchableOpacity
+        style={styles.btn}
+        onPress={funcion}>
+            <Text style={styles.text}>{texto}</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    btn:{
+      backgroundColor:'blue',
+      padding:15,
+      width:150, marginBottom:5
+    }, 
+    text:{
+      textAlign:'center', 
+      fontWeight:'900'
+    }
+  });
